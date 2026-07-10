04d17c7c0b2170dcb4fab9b8cf937561d0d66f4b
Vishal Kashyap <vk965151@gmail.com>
2026-07-10 19:38:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (74.22%) - LeetSync

---
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int originalNum = num;
+        int count = 0;
+        while(num > 0){
+            int digit = num%10;
+
+            if(originalNum % digit == 0){
+                count++;
+            }
+            num = num/10;
+        }
+        return count;
+    }
+};
