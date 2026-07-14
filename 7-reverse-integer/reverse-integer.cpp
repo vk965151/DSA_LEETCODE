0cf364e1f9290894277b5120f3ad83283ac55739
Vishal Kashyap <vk965151@gmail.com>
2026-07-14 22:37:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.91%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long reverse = 0;
+        while(x!=0){
+            int last = x%10;
+            reverse = reverse*10+last;
+            x = x/10;
+        }
+        if(reverse>INT_MAX || reverse<INT_MIN){
+            return 0;
+        }
+
+        return reverse;
+    }
+};
